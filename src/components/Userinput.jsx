795f65d2818d8216209b44cb4eb928597797fae2
unknown <ibfatade18@gmail.com>
2025-08-16 15:45:53 +0100

    Project setup

---
+++ src/components/Userinput.jsx
@@ -0,0 +1,36 @@
+export default function Userinput(){
+    return(
+        <section id="user-input">
+            <div className="input-group">
+                <p>
+                    <label>Initial Investment</label>
+                    <input />
+                </p>
+                <p>
+                    <label>Annual Investment</label>
+                    <input />
+                </p>
+            </div>
+            <div className="input-group">
+                <p>
+                    <label>Initial Investment</label>
+                    <input type="number" required />
+                </p>
+                <p>
+                    <label>Annual Investment</label>
+                    <input type="number" required />
+                </p>
+            </div>
+            <div className="input-group">
+                <p>
+                    <label>Expected Return</label>
+                    <input type="number" required />
+                </p>
+                <p>
+                    <label>Duration</label>
+                    <input type="number" required />
+                </p>
+            </div>
+        </section>
+    )
+}
